d4edb56472a4f7717c4bdabfade023b1eebad1f3
GulhermeC <gjscraveiro@ua.pt>
2024-05-27 16:46:19 +0100

    reorganize

--- rowSort/main.cu
+++ rowSort/main.cu
@@ -4,83 +4,28 @@
 #include <cuda_runtime.h>
 #include "common.h"
 
+/* allusion to internal functions */
+
+static double get_delta_time(void);
+
+__device__ void swap(int* a, int* b);
+
+__device__ void iterative_bitonic_sort(int* arr, int n,int dir,int initialK);
+
+__global__ void bitonicSort(int *seq, int N, int K, int iters,int dir);
+
 /**
-* \brief Get the process time that has elapsed since last call of this time.
-*
-* \return process elapsed time
-*/
-static double get_delta_time(void)
-{
-    static struct timespec t0, t1;
-    t0 = t1;
-    if (clock_gettime(CLOCK_MONOTONIC, &t1) != 0)
-    {
-        perror("clock_gettime");
-        exit(1);
-    }
-    return (double)(t1.tv_sec - t0.tv_sec) +
-           1.0e-9 * (double)(t1.tv_nsec - t0.tv_nsec);
-}
-
-__device__ void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-__device__ void iterative_bitonic_sort(int* arr, int n,int dir,int initialK) {
-    for (int k = initialK; k <= n; k <<= 1) {
-        for (int j = k; j > 1; j >>= 1) {
-            int z = 0;
-            for (int i = 0; i < n/j; i ++) {
-                if (z >= k) {
-                    z = 0;
-                    dir ^= 1;
-                }
-
-                for (int x = i * j; x < i * j + (j / 2); x++) {
-                    z += 2;
-                    if (dir == (arr[x] > arr[x + (j / 2)])) {
-                        swap(&arr[x], &arr[x + j / 2]);
-                    }
-                }
-            }
-        }
-    }
-}
-
-__global__ void bitonicSort(int *seq, int N, int K, int iters,int dir) {
-    int x = threadIdx.x + blockDim.x * blockIdx.x;
-    int y = threadIdx.y + blockDim.y * blockIdx.y;
-    int idx = gridDim.x * blockDim.x * y + x;
-
-    int _dir = dir;
-    int iter = 0;
-    int size = (N / K) * (1 << iter);
-    int initialK = 2;
-
-    for(int iter = 0; size <= N; iter++) {
-        int limit = (K >> iter);
-        if (idx >= limit)
-            return;
-
-        size = (N / K) * (1 << iter);
-        int* subseq = seq + size * idx;
-
-        _dir = (idx % 2 == 0) ? dir : (dir ^ 1);
-        iterative_bitonic_sort(subseq,size,_dir,initialK);
-        initialK = size << 1;
-        __syncthreads();
-    }
-
-}
+ *   main program
+ */
 
 int main(int argc,char* argv[]) {
     if(argc != 3) {
         fprintf(stderr,"Invalid number of arguments");
         return 1;
     }
-    // set up device
+
+    /* set up the device */
+
     int dev = 0;
 
     cudaDeviceProp deviceProp;
@@ -113,8 +58,13 @@ int main(int argc,char* argv[]) {
     CHECK(cudaGetLastError());      // check for kernel errors // por sempre
     printf("time elapsed %.5f\n",get_delta_time());
 
+    /* free device global memory */
+
     CHECK (cudaMemcpy (h_c, d_c, nBytes, cudaMemcpyDeviceToHost));
     CHECK (cudaFree (d_c)); //gpu
+
+    /* reset the device */
+
     CHECK (cudaDeviceReset ());
 
     int i = 0;
@@ -131,3 +81,88 @@ int main(int argc,char* argv[]) {
     return 0;
 }
 
+/**
+* \brief Get the process time that has elapsed since last call of this time.
+*
+* \return process elapsed time
+*/
+static double get_delta_time(void)
+{
+    static struct timespec t0, t1;
+    t0 = t1;
+    if (clock_gettime(CLOCK_MONOTONIC, &t1) != 0)
+    {
+        perror("clock_gettime");
+        exit(1);
+    }
+    return (double)(t1.tv_sec - t0.tv_sec) +
+           1.0e-9 * (double)(t1.tv_nsec - t0.tv_nsec);
+}
+
+/**
+ * \brief Swap the values of two integers.
+ *
+ * \param a Pointer to the first integer
+ * \param b Pointer to the second integer
+ */
+__device__ void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+__device__ void iterative_bitonic_sort(int* arr, int n,int dir,int initialK) {
+    for (int k = initialK; k <= n; k <<= 1) {
+        for (int j = k; j > 1; j >>= 1) {
+            int z = 0;
+            for (int i = 0; i < n/j; i ++) {
+                if (z >= k) {
+                    z = 0;
+                    dir ^= 1;
+                }
+
+                for (int x = i * j; x < i * j + (j / 2); x++) {
+                    z += 2;
+                    if (dir == (arr[x] > arr[x + (j / 2)])) {
+                        swap(&arr[x], &arr[x + j / 2]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * \brief Perform bitonic sort on the array.
+ *
+ * \param seq The array to be sorted
+ * \param N The total number of elements in the array
+ * \param K The number of rows
+ * \param iters The number of iterations for the bitonic sort
+ * \param dir The sorting direction (1 for ascending, 0 for descending)
+ */
+__global__ void bitonicSort(int *seq, int N, int K, int iters,int dir) {
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int idx = gridDim.x * blockDim.x * y + x;
+
+    int _dir = dir;
+    int iter = 0;
+    int size = (N / K) * (1 << iter);
+    int initialK = 2;
+
+    for(int iter = 0; size <= N; iter++) {
+        int limit = (K >> iter);
+        if (idx >= limit)
+            return;
+
+        size = (N / K) * (1 << iter);
+        int* subseq = seq + size * idx;
+
+        _dir = (idx % 2 == 0) ? dir : (dir ^ 1);
+        iterative_bitonic_sort(subseq,size,_dir,initialK);
+        initialK = size << 1;
+        __syncthreads();
+    }
+
+}
